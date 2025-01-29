9b8ef4848af064a741d3e3085fb6185efc93a592
LesleyKiprono <lesleychirchir@gmail.com>
2025-01-29 17:57:39 +0300

    data types in python

---
+++ 1_data_types.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#Data types
+
+#snake case
+maths_grade = "A"
+physics_grade = "B"
+
+#camel case
+biologyGrade ="B"
+
+print("======================================")
+print("My maths grade is:",maths_grade)
+print("My physics grade is:",physics_grade)
+print("======================================")
+
+#string: a collection of characters
+
+#integers 
+age = 24
+distance =240
+
+print("I am {} years old".format(age))
+
+print("The distance from Nairobi to Mombasa is{0} km".format(distance))
+
+
+#floating points
+height = 167
+weight = 66
+bmi = weight /(height * height)
+
+print("Your weight is : {0} kgs and height is : {1} cm and a bmi of :{2}".format(weight,height,bmi))
+
--- hello.py
+++ hello.py
@@ -4,4 +4,24 @@ print("Hello from bob")
 
 print("I am from Eldoret")
 
+#this is a single line comment
+name = "Lesley"
+age = 19
+city = "Nairobi"
+gender = "Male"
+
+print(name)
+print(age)
+print(city)
+print(gender)
+
+#strings 
+#integers
+#characters
+#floating point
+#boolean
+
+grade = "A"
+print("My KCSE grade is " ,grade)
+
 
